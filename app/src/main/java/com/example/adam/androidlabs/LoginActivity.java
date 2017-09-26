3aa8e655f8abc8f8794313366969889c10f6b95a
Adam <adam.tremblett7@gmail.com>
2017-09-26 18:03:51 -0400

    Corrected email address error

--- app/src/main/java/com/example/adam/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/adam/androidlabs/LoginActivity.java
@@ -1,13 +1,92 @@
 package com.example.adam.androidlabs;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
+    protected static  final String ACTIVITY_NAME = "LoginActivity";
+    Button loginBtn;
+    public static final String PREFS = "MyPrefsFile";
+    EditText txtEmail;
+    public static final String KEY_EMAIL = "email@domain.com";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+        // Email field
+        txtEmail = findViewById(R.id.email);
+
+        // Login Button
+        loginBtn = findViewById(R.id.loginBtn);
+
+
+        final SharedPreferences sharedPreferences = getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+
+
+
+        // Login Button Click event
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                sharedPreferences.getString("DefaultEmail", "email@domain.com");
+
+                String emailAddress = txtEmail.getText().toString();
+                editor.putString("Email", emailAddress);
+                editor.commit();
+
+
+                // Get Email from EditText
+
+
+
+            }
+        });
+
+        Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+        startActivity(intent);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
 }
